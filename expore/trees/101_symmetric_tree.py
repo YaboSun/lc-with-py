1636a7fc3bc5a9a3fb0165d1fed7282466291509
YaboSun <yabosun@163.com>
2020-07-28 23:48:46 +0800

    101_symmetric_tree

---
+++ expore/trees/101_symmetric_tree.py
@@ -0,0 +1,59 @@
+"""
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+Follow up: Solve it both recursively and iteratively.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# 递归解法
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        else:
+            return self.isMirror(root.left, root.right)
+
+    """
+    定义判定是否为镜像
+    """
+
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left.val == right.val:
+            outPair = self.isMirror(left.left, right.right)
+            inPair = self.isMirror(left.right, right.left)
+            return outPair and inPair
+        else:
+            return False
+
+# # 迭代解法
+
