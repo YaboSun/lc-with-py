1ce6972a5c2828a2d4451bbbccc1f605b079adbb
YaboSun <yabosun@163.com>
2020-08-14 07:48:38 +0800

    326_Power_of_Three

---
+++ expore/math/326_Power_of_Three.py
@@ -0,0 +1,30 @@
+"""
+Given an integer, write a function to determine if it is a power of three.
+
+Example 1:
+
+Input: 27
+Output: true
+Example 2:
+
+Input: 0
+Output: false
+Example 3:
+
+Input: 9
+Output: true
+Example 4:
+
+Input: 45
+Output: false
+"""
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and 1162261467 % n == 0
+
+
+if __name__ == '__main__':
+    so = Solution()
+    print(so.isPowerOfThree(9))
