78e44ad205e475252a60d169ca1b87490fb6574c
YaboSun <yabosun@163.com>
2022-04-10 15:32:34 +0800

    随想录-array03-977.有序数组的平方

--- suixianlu/array/977.有序数组的平方.py
+++ suixianlu/array/977.有序数组的平方.py
@@ -18,27 +18,46 @@ class Solution:
     # 暴力解法
     # def sortedSquares(self, nums: List[int]) -> List[int]:
     #     for i in range(len(nums)):
-    #         nums[i] = nums[i] * nums[i]
+    #         nums[i] = nums[i] ** 2
     #
     #     return sorted(nums)
-
-    # 双指针，开辟新数组空间存储
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        ret_nums = [None] * len(nums)
-        left_idx, right_idx = 0, len(nums) - 1
-        tmp = right_idx
-        while left_idx <= right_idx:
-            if nums[left_idx] * nums[left_idx] < nums[right_idx] * nums[right_idx]:
-                ret_nums[tmp] = nums[right_idx] * nums[right_idx]
-                right_idx = right_idx - 1
-            else:
-                ret_nums[tmp] = nums[left_idx] * nums[left_idx]
-                left_idx = left_idx + 1
-            tmp = tmp - 1
-
-        return ret_nums
-
     #
+    # # 双指针，开辟新数组空间存储
+    # def sortedSquares(self, nums: List[int]) -> List[int]:
+    #     ret_nums = [None] * len(nums)
+    #     left_idx, right_idx = 0, len(nums) - 1
+    #     tmp = right_idx
+    #     while left_idx <= right_idx:
+    #         left = nums[left_idx] ** 2
+    #         right = nums[right_idx] ** 2
+    #         if left < right:
+    #             ret_nums[tmp] = right
+    #             right_idx = right_idx - 1
+    #         else:
+    #             ret_nums[tmp] = left
+    #             left_idx = left_idx + 1
+    #         tmp = tmp - 1
+    #
+    #     return ret_nums
+
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        # 可以利用一下非递减的特性 统计小于0的个数 然后拆分为三段
+        # 有点繁琐
+        a = 0
+        length = len(nums)
+        while a < length and nums[a] < 0:
+            a += 1
+
+        left = [-e for e in nums[:a][::-1]]
+        min1 = nums[0]
+        b = a
+        if min1 < 0:
+            while b < length and nums[b] < -min1:
+                b += 1
+        right = nums[b:]
+
+        left.extend(nums[a:b])
+        return [e**2 for e in sorted(left)] + [e**2 for e in right]
 
 
 if __name__ == '__main__':
